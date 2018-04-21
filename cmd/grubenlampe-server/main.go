e4167b2563cbe4a16f8a6f3cf046b46db0bb2a17
Markus Lindenberg <markus@lindenberg.io>
2018-04-21 13:03:53 +0200

    Add grubenlampe-server main boilerplate

---
+++ cmd/grubenlampe-server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"os"
+
+	// pb "github.com/ffrl/grubenlampe/api"
+	// "github.com/ffrl/grubenlampe/database"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func init() {
+	log.SetFlags(0)
+}
+
+func main() {
+	var (
+		listenAddress = flag.String("listen", ":20170", "GRPC listener host:port")
+		driver        = flag.String("db", "sqlite3", "Database driver (sqlite3, mysql or postgres)")
+		dsn           = flag.String("dsn", "grubenlampe.db", "Database DSN (GRUBENLAMPE_DSN)")
+	)
+	flag.Parse()
+
+	if envDSN := os.Getenv("GRUBENLAMPE_DSN"); envDSN != "" {
+		*dsn = envDSN
+	}
+
+	db, err := gorm.Open(*driver, *dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.AutoMigrate()
+
+	lis, err := net.Listen("tcp", *listenAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
