240a5f29686bf206017921cf9353e1b2c31ac3e9
Daniel Czerwonk <daniel@dan-nrw.de>
2018-04-22 14:25:58 +0200

    added org and user FKs

--- database/log.go
+++ database/log.go
@@ -7,10 +7,16 @@ type Log struct {
 	gorm.Model
 
 	// User is the user requested the operation
-	User *User
+	User *User `gorm:"foreignkey:UserID"`
+
+	// UserID is the foreign key to an user
+	UserID uint `gorm:"nullable"`
 
 	// Org is the organisation on which behalv the user requested the action
-	Org *Org
+	Org *Org `gorm:"foreignkey:OrgID"`
+
+	// OrgID is the foreign key to an orgnaisation
+	OrgID uint `gorm:"nullable"`
 
 	// Request is the serialized string representation of the request
 	RequestMessage string
