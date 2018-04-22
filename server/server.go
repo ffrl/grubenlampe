09996f4c1eb59e173be42e3d391565568ba55ad3
Oliver Herms <oliver.herms@exaring.de>
2018-04-22 11:05:32 +0200

    Extending error messages and adding documentation

--- server/server.go
+++ server/server.go
@@ -29,7 +29,7 @@ func (s *Server) AddUser(ctx context.Context, req *pb.AddUserRequest) (*pb.Gener
 	users := s.db.Users()
 	exists, err := users.EmailExists(req.Email)
 	if err != nil {
-		return nil, fmt.Errorf("error while processing")
+		return nil, fmt.Errorf("error while processing: %v", err)
 	}
 	if exists {
 		return &pb.GenericResponse{Message: "User already exists"}, nil
@@ -42,17 +42,18 @@ func (s *Server) AddUser(ctx context.Context, req *pb.AddUserRequest) (*pb.Gener
 	}
 	err = users.Save(u)
 	if err != nil {
-		return nil, fmt.Errorf("could not store user")
+		return nil, fmt.Errorf("could not store user: %v", err)
 	}
 
 	return &pb.GenericResponse{Success: true}, nil
 }
 
+// AddOrg creates an organization
 func (s *Server) AddOrg(ctx context.Context, req *pb.AddOrgRequest) (*pb.GenericResponse, error) {
 	orgs := s.db.Orgs()
 	exists, err := orgs.ShortNameExists(req.ShortName)
 	if err != nil {
-		return nil, fmt.Errorf("error while processing")
+		return nil, fmt.Errorf("error while processing: %v", err)
 	}
 	if exists {
 		return &pb.GenericResponse{Message: "Org already exists"}, nil
@@ -64,23 +65,24 @@ func (s *Server) AddOrg(ctx context.Context, req *pb.AddOrgRequest) (*pb.Generic
 	}
 	err = orgs.Save(o)
 	if err != nil {
-		return nil, fmt.Errorf("could not store org")
+		return nil, fmt.Errorf("could not store org: %v", err)
 	}
 
 	return &pb.GenericResponse{Success: true}, nil
 }
 
+// AddASN creates an Autonomous System Number
 func (s *Server) AddASN(ctx context.Context, req *pb.AddASNRequest) (*pb.GenericResponse, error) {
 	orgs := s.db.Orgs()
 	o, err := orgs.GetByShortName(req.OrgShortName)
 	if err != nil {
-		return nil, fmt.Errorf("error while processing")
+		return nil, fmt.Errorf("error while processing: %s", err)
 	}
 
 	asns := s.db.ASNs()
 	exists, err := asns.CheckedASNExists(req.Asn)
 	if err != nil {
-		return nil, fmt.Errorf("error while processing")
+		return nil, fmt.Errorf("error while processing: %v", err)
 	}
 	if exists {
 		return &pb.GenericResponse{Message: "ASN already exists"}, nil
@@ -92,40 +94,48 @@ func (s *Server) AddASN(ctx context.Context, req *pb.AddASNRequest) (*pb.Generic
 	}
 	err = asns.Save(a)
 	if err != nil {
-		return nil, fmt.Errorf("could not store ASN")
+		return nil, fmt.Errorf("could not store ASN: %v", err)
 	}
 
 	return &pb.GenericResponse{Success: true}, nil
 }
 
+// AddTunnel crates a tunnel
 func (s *Server) AddTunnel(context.Context, *pb.AddTunnelRequest) (*pb.GenericResponse, error) {
 	return &pb.GenericResponse{Success: false, Message: "not implemented"}, nil
 }
 
+// DeleteTunnel deletes a tunnel
 func (s *Server) DeleteTunnel(context.Context, *pb.DeleteTunnelRequest) (*pb.GenericResponse, error) {
 	return &pb.GenericResponse{Success: false, Message: "not implemented"}, nil
 }
 
+// AddIPv4Address creates an IPv4 address
 func (s *Server) AddIPv4Address(context.Context, *pb.AddIPv4AddressRequest) (*pb.AddIPv4AddressResponse, error) {
 	return nil, nil
 }
 
+// ReleaseIPv4Address releases an IPv4 address
 func (s *Server) ReleaseIPv4Address(context.Context, *pb.ReleaseIPv4AddressRequest) (*pb.GenericResponse, error) {
 	return nil, nil
 }
 
+// AddIPv6Prefix creates an IPv6 /48 prefix
 func (s *Server) AddIPv6Prefix(context.Context, *pb.AddIPv6PrefixRequest) (*pb.AddIPv6PrefixReply, error) {
 	return nil, nil
 }
 
+// ReleaseIPv6Prefix releases an IPv6 prefix
 func (s *Server) ReleaseIPv6Prefix(context.Context, *pb.ReleaseIPv6PrefixRequest) (*pb.GenericResponse, error) {
 	return nil, nil
 }
 
+// GetBGPStatus gets status of BGP sessions
 func (s *Server) GetBGPStatus(context.Context, *pb.GetBGPStatusRequest) (*pb.GetBGPStatusReply, error) {
 	return nil, nil
 }
 
+// GetLogs gets Logs
 func (s *Server) GetLogs(context.Context, *pb.GetLogsRequest) (*pb.GetLogsReply, error) {
 	return nil, nil
 }
