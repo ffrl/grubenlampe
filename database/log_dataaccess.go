42d3fbe4611e73737ef252a0cae08a07d4db0027
Daniel Czerwonk <daniel@dan-nrw.de>
2018-04-22 16:30:22 +0200

    simplification

--- database/log_dataaccess.go
+++ database/log_dataaccess.go
@@ -7,10 +7,5 @@ type LogDataAccess struct {
 
 // Insert inserts a log entry
 func (d *LogDataAccess) Insert(log *Log) error {
-	err := d.conn.db.Create(log).Error
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return d.conn.db.Create(log).Error
 }
